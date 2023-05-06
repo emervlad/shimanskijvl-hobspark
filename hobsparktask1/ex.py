8875d0bea431ccb377b2216c037c20dae82acc92
shimanskijvl <hob2023026@mipt-client.atp-fivt.org>
2023-05-06 20:38:17 +0000

    Remove comments

--- hobsparktask1/ex.py
+++ hobsparktask1/ex.py
@@ -26,17 +26,14 @@ distances = sc.parallelize([(x, (d, []))]).partitionBy(n)
 
 while True:
   candidates = distances.join(forward_edges, n).map(step)
-  #print(candidates.collect())
+
   keys = distances.keys().collect()
-  #print(keys)
-  #print(forward_edges.collect())
+
   forward_edges = forward_edges.filter(lambda i: i[0] not in keys and i[1] not in keys)
   new_distances = distances.fullOuterJoin(candidates, n).map(complete, True).persist()
   count = new_distances.filter(lambda i: i[0] == 34).count()
   if count == 0:
     distances = new_distances.filter(lambda i: i[1][0] > d - 1)
-    #print(distances.collect())
-    #print(map(lambda x: x[1][0] - d - 1, distances.collect()))
     d += 1
   else:
     break
